d9544217b0dbb24962798ccbc3b1fa82e0c15fd2
Naagma <naagma5@gmail.com>
2023-02-22 10:52:54 -0600

    update

--- 03-critical-design/start/assets/js/main.js
+++ 03-critical-design/start/assets/js/main.js
@@ -3,40 +3,40 @@
 
 
 // 2
-document.getElementById("ele2").addEventListener("click", function () {
-  if (this.src.includes("cat-01")){
-    this.src = "assets/img/cat-02.jpg";
+document.getElementById("#ele2").addEventListener("click", function () {
+  if (this.src.includes("BetterHelp.svg")){
+    this.src = "assets/img/better_hellp.png";
   } else {
-    this.src = "assets/img/cat-01.jpg";
+    this.src = "assets/img/BetterHelp.svg";
   }
 });
 
 // 3
 let ele3 = document.querySelector("#ele3");
 ele3.addEventListener("click", function () {
-  if (this.src.includes("cat-01")){
-    this.src = "assets/img/cat-02.jpg";
+  if (this.src.includes("BetterHelp.svg")){
+    this.src = "assets/img/better_hellp.png";
   } else {
-    this.src = "assets/img/cat-01.jpg";
+    this.src = "assets/img/BetterHelp.svg";
   }
 });
 
 // 4
 let ele4 = document.querySelector("#ele4");
 ele4.addEventListener("mouseover", function () {
-  this.src = "assets/img/cat-02.jpg";
+  this.src = "assets/img/better_hellp.png";
 });
 ele4.addEventListener("mouseout", function () {
-  this.src = "assets/img/cat-01.jpg";
+  this.src = "assets/img/BetterHelp.svg";
 });
 
 // 5
 let ele5 = document.querySelector("#ele5");
 ele5.addEventListener("mouseover", function () {
-  this.style.backgroundImage = "url(assets/img/cat-02.jpg)";
+  this.style.backgroundImage = "url(assets/img/better_hellp.png)";
 });
 ele5.addEventListener("mouseout", function () {
-  this.style.backgroundImage = "url(assets/img/cat-01.jpg)";
+  this.style.backgroundImage = "url(assets/img/BetterHelp.svg)";
 });
 
 
